5a922b116f11d60467e8df377aa1462f7cca4056
toleikis1990 <toleikis.tomas@gmail.com>
2021-02-20 12:47:36 +0200

    add composition examples

---
+++ JavaFundamentals/src/advanced/composition/Bed.java
@@ -0,0 +1,13 @@
+package advanced.composition;
+
+public class Bed {
+
+    private String name;
+    private String bedroomType;
+
+    public Bed(String name, String beroomType){
+        this.name = name;
+        this.bedroomType = beroomType;
+    }
+
+}

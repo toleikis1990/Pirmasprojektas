cb8deb3de2d2149e282adb6b9e7c74853798564c
toleikis1990 <toleikis.tomas@gmail.com>
2021-03-07 11:32:59 +0200

    add testing examples

---
+++ src/main/java/fundamentals_testing/Calculator.java
@@ -0,0 +1,20 @@
+package fundamentals_testing;
+
+public class Calculator {
+
+    public int add(int firstNum, int secondNum){
+        return firstNum + secondNum;
+    }
+
+    public int subtract(int firstNum, int secondNum){
+        return firstNum - secondNum;
+    }
+
+    public int daugyba(int firstNum, int secondNum){
+        return firstNum * secondNum;
+    }
+
+    public double dalyba(double firstNum, double secondNum){
+        return firstNum / secondNum;
+    }
+}
